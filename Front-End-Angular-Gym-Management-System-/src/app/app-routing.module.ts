638227867e4821933ef781f1a64c3177b4743ff6
unknown <alhousany1241997@gmail.com>
2022-07-25 19:31:38 +0200

    add shop

--- Front-End-Angular-Gym-Management-System-/src/app/app-routing.module.ts
+++ Front-End-Angular-Gym-Management-System-/src/app/app-routing.module.ts
@@ -4,13 +4,11 @@ import { BodyComponent } from './components/body/body.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-// const routes: Routes = [
 
-// ];
 
 const routes: Routes = [{path:"",component: MainLayoutComponent,children:[
-  {path:"",redirectTo:'/Home',pathMatch:'full'},//Default Path
-  {path:'Home',component:HomeComponent},
+  {path:"",redirectTo:'home',pathMatch:'full'},//Default Path
+  {path:'home',component:HomeComponent},
   {path:'contact',component:BodyComponent},
 
 ]},{path:'**',component:NotFoundComponent},
