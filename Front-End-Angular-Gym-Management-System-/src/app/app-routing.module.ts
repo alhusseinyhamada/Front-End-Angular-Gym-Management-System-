eaccc95606a2c57f04964b0536a031a2490295fb
kero-edward <kyrillos.edward17@gmail.com>
2022-07-26 04:16:19 +0200

    classes

--- Front-End-Angular-Gym-Management-System-/src/app/app-routing.module.ts
+++ Front-End-Angular-Gym-Management-System-/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
+import { ClassComponent } from './components/class/class.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+// const routes: Routes = [
 
+// ];
 
 const routes: Routes = [{path:"",component: MainLayoutComponent,children:[
   {path:"",redirectTo:'home',pathMatch:'full'},//Default Path
   {path:'home',component:HomeComponent},
   {path:'contact',component:BodyComponent},
-
+  {path:'classes',component:ClassComponent},
 ]},{path:'**',component:NotFoundComponent},
 ];
 
--- Front-End-Angular-Gym-Management-System-/src/app/app.module.ts
+++ Front-End-Angular-Gym-Management-System-/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BodyComponent } from './components/body/body.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ClassComponent } from './components/class/class.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     BodyComponent,
     MainLayoutComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ClassComponent
   ],
   imports: [
     BrowserModule,
